67b746076d8b931421a728e02af50c1460d8826b
Lucas Proudhon Smith <75811985+lucas-ps@users.noreply.github.com>
2023-03-10 16:06:21 +0000

    first commit

--- test_scripts/script1.sh
+++ test_scripts/script1.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-ID="Everybody+(Backstreet's+Back)+(Radio+Edit)"
+ID="Everybody+(Backstreets+Back)+(Radio+Edit)"
 ESCAPED=`perl -e "use URI::Escape; print uri_escape(\"$ID\")"`
 AUDIO=`base64 -i "$ID".wav`
 RESOURCE=localhost:3000/tracks/$ESCAPED
