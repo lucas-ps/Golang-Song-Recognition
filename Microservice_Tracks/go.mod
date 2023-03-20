f611094df464cc7aa6a7c9cbbfc92bea3e0a7c30
Lucas Proudhon Smith <75811985+lucas-ps@users.noreply.github.com>
2023-03-20 18:12:14 +0000

    Restructured project

--- Microservice_Tracks/go.mod
+++
@@ -1,8 +0,0 @@
-module main.go
-
-go 1.19
-
-require (
-	github.com/gorilla/mux v1.8.0
-	github.com/mattn/go-sqlite3 v1.14.16
-)
--- test_scripts/script5.sh
+++ test_scripts/script5.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-ID="~Everybody+(Backstreet's+Back)+(Radio+Edit)"
+ID="b"
 AUDIO=`base64 -i "$ID".wav`
 RESOURCE=localhost:3001/search
 echo "{ \"Audio\":\"$AUDIO\" }" > input
